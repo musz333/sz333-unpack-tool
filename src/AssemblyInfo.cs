b08c32f0137bd762befc46e10f36aea7a2d4983f
musz333 <1303592964@qq.com>
2026-08-21 23:56:16 +0800

    Upload src/AssemblyInfo.cs

---
+++ src/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("分卷识别 · 多层解压 · 自动去重 · 自动清理 | 本软件完全免费，如通过购买获得，请立即退款 | 无需赞赏，愿天下开源")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyProduct("sz333解压工具")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyCompany("sz333")]
+[assembly: AssemblyCopyright("Created by sz333")]
+[assembly: AssemblyTitle("sz333解压工具")]
+[assembly: AssemblyVersion("1.1.0.0")]
